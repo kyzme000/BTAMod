b97ba2f271e97ece31f46d59c4e3b76479a71ad5
User <kuzme222@gmail.com>
2025-08-01 20:03:51 +0300

    mixin fix

--- src/main/java/kuzme/kaifcraft/mixin/MobCreeperMixin.java
+++ src/main/java/kuzme/kaifcraft/mixin/MobCreeperMixin.java
@@ -6,13 +6,11 @@ import net.minecraft.core.entity.monster.MobCreeper;
 import net.minecraft.core.entity.monster.MobMonster;
 import net.minecraft.core.util.phys.AABB;
 import net.minecraft.core.world.World;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
@@ -27,18 +25,14 @@ public abstract class MobCreeperMixin extends MobMonster {
 	@Shadow private int timeSinceIgnited;
 	@Shadow public abstract void setCreeperState(int state);
 
-	@Redirect(
-		method = "attackEntity",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/core/world/World;createExplosion(Lnet/minecraft/core/entity/Entity;DDDF)V",
-			ordinal = 0
-		)
+	@Inject(
+		method = "attackEntity(Lnet/minecraft/core/entity/Entity;F)V",
+		at = @At("HEAD"),
+		cancellable = true
 	)
 	private void attackEntity(Entity entity, float distance, CallbackInfo ci) {
 		System.out.println("Mixin injected!");
 		if (isSmoked()) {
-
 			this.setCreeperState(-1);
 			this.timeSinceIgnited = 0;
 			ci.cancel();  // отменяем вызов createExplosion и дальнейшее выполнение
